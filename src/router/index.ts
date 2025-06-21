fdb9254754a2a90e5141ea34b81db202f7fe4b6e
Patrick Simões <patricksimoes25@gmail.com>
2025-06-21 15:33:00 -0300

    feat: Implements base fleet

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: () => import(''),
-    // },
+    {
+      path: '/',
+      name: 'fleet',
+      component: () => import('../components/FleetSection.vue'),
+    },
   ],
 })
 
