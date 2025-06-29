70720c84c0157f4f3d4574b42b95836a018edd9f
Patrick Simões <patricksimoes25@gmail.com>
2025-06-29 11:12:30 -0300

    fix: remove text info

--- src/components/WhatsAppFloatingButton.vue
+++ src/components/WhatsAppFloatingButton.vue
@@ -1,12 +1,7 @@
 <template>
-  <a
-    :href="whatsappLink()"
-    target="_blank"
-    rel="noopener noreferrer"
-    class="fixed bottom-6 right-6 z-50 flex items-center gap-2 bg-green-500 text-white px-4 py-3 rounded-full shadow-lg hover:bg-green-600"
-  >
+  <a :href="whatsappLink()" target="_blank" rel="noopener noreferrer"
+    class="fixed bottom-6 right-6 z-50 flex items-center gap-2 bg-green-500 text-white px-4 py-3 rounded-full shadow-lg hover:bg-green-600">
     <img src="@/assets/social-media/WhatsApp.svg.png" alt="WhatsApp" class="w-6 h-6" />
-    <span class="font-semibold">Info</span>
   </a>
 </template>
 
